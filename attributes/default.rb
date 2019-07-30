99a529e258cd5ba663166e49a1105ae052d03e7b
Jay Flowers <jay.flowers@gmail.com>
2019-07-30 14:31:08 -0400

    update to latest versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 
 
 
-default[:kube][:minikube][:version] = '0.23.0'
-default[:kube][:minikube][:checksum] = '3d0c5581cd14f85637fb888c1e2e124152c4c9643a257ba90c8cd929d2c2b8b3'
+default[:kube][:minikube][:version] = '1.2.0'
+default[:kube][:minikube][:checksum] = '183d017d094b7783c938dc709dbdfc9a48f92299178234f89047dfbb083a592c'
 default[:kube][:minikube][:base_directory] = '/usr/local/kubernetes/minikube'
 
-default[:kube][:kubectl][:version] = '1.8.3'
-default[:kube][:kubectl][:checksum] = '523bd724529dc84e0d1cc7610688566ccef599e0f2eadfbb2edb8332661d355c'
+default[:kube][:kubectl][:version] = '1.15.1'
+default[:kube][:kubectl][:checksum] = '63f1ace419edffa1f5ebb64a6c63597afd48f8d94a61d4fb44e820139adbbe54'
 default[:kube][:kubectl][:base_directory] = '/usr/local/kubernetes/kubectl'
