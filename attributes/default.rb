57de74005e0e732f8c4c3837b725a65399246d5d
Jay Flowers <jay.flowers@gmail.com>
2019-07-30 16:30:39 -0400

    fix sha

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,5 @@ default[:kube][:minikube][:checksum] = '183d017d094b7783c938dc709dbdfc9a48f92299
 default[:kube][:minikube][:base_directory] = '/usr/local/kubernetes/minikube'
 
 default[:kube][:kubectl][:version] = '1.15.1'
-default[:kube][:kubectl][:checksum] = '63f1ace419edffa1f5ebb64a6c63597afd48f8d94a61d4fb44e820139adbbe54'
+default[:kube][:kubectl][:checksum] = '7d988cd758b8f46721430454f2cc94d511112f183a5ebab9a84a8a8f1d14b36d'
 default[:kube][:kubectl][:base_directory] = '/usr/local/kubernetes/kubectl'
