541de60c418b6ceb69465ad60fa019518176459f
Jay Flowers <jay.flowers@gmail.com>
2019-07-31 10:32:08 -0400

    commit for version : 0.1.14

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jay.flowers@gmail.com'
 license          'Apache License 2.0'
 description      'Installs/Configures kube'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.11'
+version          '0.1.14'
 
 depends 'tar', '~> 2.2.0'
