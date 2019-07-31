3d15c62f9f2e2e08a665784fb2d42a05dca5951c
Jay Flowers <jay.flowers@gmail.com>
2019-07-31 10:31:45 -0400

    untar helm

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'Apache License 2.0'
 description      'Installs/Configures kube'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.11'
+
+depends 'tar', '~> 2.2.0'
--- recipes/helm.rb
+++ recipes/helm.rb
@@ -6,17 +6,17 @@
 download_url = "https://get.helm.sh/helm-v#{node[:kube][:helm][:version]}-darwin-amd64.tar.gz"
 
 helm_directory = "#{node[:kube][:helm][:base_directory]}/#{node[:kube][:helm][:version]}"
-helm_file_path = "#{helm_directory}/helm"
+helm_file_path = "#{helm_directory}/darwin-amd64/helm"
 
 directory helm_directory do
   owner ENV['USER']
   recursive true
 end
 
-remote_file helm_file_path do
-  source download_url
-  mode '0755'
-  owner ENV['USER']
+tar_extract download_url do
+  target_dir helm_directory
+  creates helm_file_path
+  user ENV['USER']
   checksum node[:kube][:helm][:checksum]
 end
 
