ed954ba23296c2ca796d64eb1068b3cb14a6e82a
Jay Flowers <jay.flowers@gmail.com>
2019-07-31 09:39:58 -0400

    fix sha

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,5 +10,5 @@ default[:kube][:kubectl][:checksum] = '7d988cd758b8f46721430454f2cc94d511112f183
 default[:kube][:kubectl][:base_directory] = '/usr/local/kubernetes/kubectl'
 
 default[:kube][:helm][:version] = '2.14.3'
-default[:kube][:helm][:checksum] = '9469da75bb357bc325c985dead224480bbacc7aa8aecff74089dff97e64655d0'
+default[:kube][:helm][:checksum] = '3f907cac93c6b18c36910b15af02ddc3d454b59a870899afdfa75548f9a7658d'
 default[:kube][:helm][:base_directory] = '/usr/local/kubernetes/helm'
